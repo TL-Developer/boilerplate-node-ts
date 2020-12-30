31aa68c53b0043da3173e6be35aeb0c178cf5a95
Tiago Lima <tiago.lima@xpi.com.br>
2020-12-30 19:12:12 -0300

    estrutura

--- src/app/helpers/factory.ts
+++ src/app/helpers/factory.ts
@@ -1,4 +1,9 @@
-export const responseJson = (result?: []) => ({
+export const responseJson = (result?: []): {
+  success: boolean,
+  message: null | string,
+  result: [],
+  error: null | string,
+} => ({
   success: true,
   message: null,
   result: result || [],
